6f56a6e021d86ada2ad2352e775930fa85e57e9b
Mayank Sharma <202410104+Mayank-iitj@users.noreply.github.com>
2025-11-07 09:19:32 +0530

    Remove directory creation for images and CSS
    Removed directory creation for images and CSS.

--- app.py
+++ app.py
@@ -30,9 +30,6 @@ st.set_page_config(
 PATH_IMAGES = Path("./images")
 PATH_CSS = Path("./css")
 PATH_FACE_DETECTOR = Path("./face_detector")
-PATH_IMAGES.mkdir(exist_ok=True, parents=True)
-PATH_CSS.mkdir(exist_ok=True, parents=True)
-
 def local_css(file_name):
     """Method for reading styles.css and applying necessary changes to HTML"""
     css_file = Path(file_name)
